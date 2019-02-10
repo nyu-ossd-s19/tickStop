8746e3e03819e4667cf7f546cb19213d6b536e8b
Vivian Fang <vf608@nyu.edu>
2019-02-10 17:13:18 -0500

    Fix requestDetails bug

--- background.js
+++ background.js
@@ -1,9 +1,11 @@
 var DELAY = 20;
 var PERIOD = 5;
+var URL = "";
 
 function startTimer(requestDetails) {
+    URL = requestDetails.url;
     browser.alarms.create("FIRST ALARM", {delayInMinutes: DELAY});
-    console.log("Start timer for: " + requestDetails.url);
+    console.log("Start timer for: " + URL);
 }
 
 browser.webRequest.onBeforeRequest.addListener(
@@ -12,9 +14,7 @@ browser.webRequest.onBeforeRequest.addListener(
 );
 
 browser.alarms.onAlarm.addListener((alarm) => {
-    console.log("Your time is up! We'll remind you every 5 extra minutes you
-            spend on here.");
-
+    console.log("Your time is up! We'll remind you every 5 extra minutes you spend on here.");
     browser.alarms.create("SECOND ALARM", {periodInMinutes: PERIOD});
-    console.log("Second timer for: " + requestDetails.url);
+    console.log("Second timer for: " + URL);
 });
