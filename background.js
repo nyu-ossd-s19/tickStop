95b840e6ac63583e31c70441591f71d3bf596692
Rachel <rachelyhe@nyu.edu>
2019-02-10 11:32:08 -0500

    Added a second reminder timer that fires every 5 minutes

--- background.js
+++ background.js
@@ -1,14 +1,20 @@
 var DELAY = 20;
+var PERIOD = 5;
+
 function startTimer(requestDetails) {
-  browser.alarms.create("", {delayInMinutes: DELAY});
-  console.log("Start timer for: " + requestDetails.url);
+    browser.alarms.create("FIRST ALARM", {delayInMinutes: DELAY});
+    console.log("Start timer for: " + requestDetails.url);
 }
 
 browser.webRequest.onBeforeRequest.addListener(
-  startTimer,
-  {urls: ["*://www.facebook.com/*"]}
+    startTimer,
+    {urls: ["*://www.facebook.com/*"]}
 );
 
 browser.alarms.onAlarm.addListener((alarm) => {
-  console.log("End timer");
+    console.log("Your time is up! We'll remind you every 5 extra minutes you
+            spend on here.");
+
+    browser.alarms.create("SECOND ALARM", {periodInMinutes: PERIOD});
+    console.log("Second timer for: " + requestDetails.url);
 });
