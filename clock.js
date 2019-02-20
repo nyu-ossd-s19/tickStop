2181aaf50401370ff6f38f1ee582c368dbb3414a
Rachel <rachelyhe@nyu.edu>
2019-02-20 15:16:58 -0500

    Several frontend design changes: added timer status (started / paused), changed padding and font, added button styling, changed color scheme

--- clock.js
+++ clock.js
@@ -1,43 +1,41 @@
+var head = document.getElementsByTagName('h1')[0],
+start = document.getElementById('start'),
+pause = document.getElementById('pause'),
+reset = document.getElementById('reset'),
+seconds = 0, minutes = 0, hours = 0,
+t;
 
-  
-        var head = document.getElementsByTagName('h1')[0],
-        start = document.getElementById('start'),
-        stop = document.getElementById('stop'),
-        reset = document.getElementById('reset'),
-        seconds = 0, minutes = 0, hours = 0,
-        t;
-
-        function add() {
-            seconds++;
-            if (seconds >= 60) {
-                seconds = 0;
-                minutes++;
-                if (minutes >= 60) {
-                    minutes = 0;
-                    hours++;
-                }
-            }
-            
-            head.textContent = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
-
-            timer();
+function add() {
+    seconds++;
+    if (seconds >= 60) {
+        seconds = 0;
+        minutes++;
+        if (minutes >= 60) {
+            minutes = 0;
+            hours++;
         }
-        function timer() {
-            t = setTimeout(add, 1000);
-        }
-        timer();
+    }
+    head.textContent = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
+    timer();
+}
+function timer() {
+    t = setTimeout(add, 1000);
+    document.getElementById("status").style.cssText = 'background: #9DE492;';
+    document.getElementById("status").innerHTML = "STARTED";
+}
+timer();
 
+start.onclick = timer;
 
+pause.onclick = function() {
+    clearTimeout(t);
+    document.getElementById("status").style.cssText = 'background: #FF7C7C;';
+    document.getElementById("status").innerHTML = "PAUSED";
+}
 
-        start.onclick = timer;
-
-
-        stop.onclick = function() {
-            clearTimeout(t);
-        }
-
-
-        reset.onclick = function() {
-            head.textContent = "00:00:00";
-            seconds = 0; minutes = 0; hours = 0;
-        }
+reset.onclick = function() {
+    head.textContent = "00:00:00";
+    seconds = 0; minutes = 0; hours = 0;
+    document.getElementById("status").style.cssText = 'background: #F9F9F9;';
+    document.getElementById("status").innerHTML = "RESET";
+}
