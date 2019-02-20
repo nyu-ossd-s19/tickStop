dba0cf9a9ab84c0e27c6f7fc5282682e562c2922
Rachel <rachelyhe@nyu.edu>
2019-02-20 18:06:18 -0500

    New timer logic added, "START" button no longer creates multiple instances of a timer when clicked repeatedly

--- clock.js
+++ clock.js
@@ -4,6 +4,7 @@ pause = document.getElementById('pause'),
 reset = document.getElementById('reset'),
 seconds = 0, minutes = 0, hours = 0,
 t;
+var status = 'resumed';
 
 function add() {
     seconds++;
@@ -20,22 +21,30 @@ function add() {
 }
 function timer() {
     t = setTimeout(add, 1000);
-    document.getElementById("status").style.cssText = 'background: #9DE492;';
-    document.getElementById("status").innerHTML = "STARTED";
 }
 timer();
 
-start.onclick = timer;
+start.onclick = function() {
+    if(status !== 'resumed') {
+        timer();
+        status = 'resumed';
+        document.getElementById("status").style.cssText = 'background: #9DE492;';
+        document.getElementById("status").innerHTML = "RESUMED";
+    }
+};
 
 pause.onclick = function() {
     clearTimeout(t);
+    status = 'paused';
     document.getElementById("status").style.cssText = 'background: #FF7C7C;';
     document.getElementById("status").innerHTML = "PAUSED";
 }
 
 reset.onclick = function() {
+    clearTimeout(t);
     head.textContent = "00:00:00";
     seconds = 0; minutes = 0; hours = 0;
+    status = 'reset';
     document.getElementById("status").style.cssText = 'background: #D1D1D1;';
     document.getElementById("status").innerHTML = "RESET";
 }
